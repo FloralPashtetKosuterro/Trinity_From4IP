c00b78362443ace30f55a9d834c0ee9418c4daa8
Daelss <denchiklis@list.ru>
2023-09-23 18:23:35 +0300

    Калькулятор на коленке

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 #Чистая доска для развелечений. Поехали!
+simvol = int(input('Введите цифру:'))
+
+simvol2 = input('Выберите действие:+-/*:')
+
+simvol3 = int(input('Введите цифру:'))
+
+result = ''
+
+if simvol2 =='+':
+    result = simvol + simvol3
+elif simvol2=='-':
+    result = simvol - simvol3
+elif simvol2=='/':
+    result = simvol / simvol3
+elif simvol2 =='*':
+    result = simvol * simvol3
+else:
+    print('такого действия нету в калькуляторе')
+
+print('Ответ:', result)
+
